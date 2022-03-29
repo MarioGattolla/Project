6b77cea20bf7b355181d4dbf5e1cdca6b784922c
mario <mario.gattolla@gmail.com>
2022-03-29 16:58:33 +0200

    backup

--- resources/views/admin/subscriptions/create.blade.php
+++ resources/views/admin/subscriptions/create.blade.php
@@ -23,36 +23,40 @@ $search_users = User::all()
         </x-header>
 
         <script>
-
             function filterUser() {
                 return {
-                    search: "",
+                    search: '',
                     data: sourceData,
+                    selectedUserIndex: '',
                     get filteredUser() {
-                        if (this.search === "") {
-                            return this.data;
+                        if (this.search === '') {
+                            return [];
                         }
-                        return this.data.filter((item) => {
-                            return item.user_surname
-                                .toLowerCase()
-                                .includes(this.search.toLowerCase());
-                        });
+                        return this.data.filter(search => search.user_surname.toLowerCase().includes(this.search.toLowerCase()));
+
+                    },
+                    reset() {
+                        this.search = '';
+                    },
+                    selectNextUser() {
+                        if (this.selectedUserIndex === '') {
+                            this.selectedUserIndex = 0;
+                        } else {
+                            this.selectedUserIndex++;
+                        }
+                    },
+                    selectPreviousUser() {
+                        if (this.selectedUserIndex === '') {
+                            this.selectedUserIndex = 0;
+                        } else {
+                            this.selectedUserIndex--;
+                        }
                     },
                 };
             }
 
-            var sourceData = [
-                {
-                    id: "1",
-                    user_surname: "Tiger Nixon",
-                },
-                {
-                    id: "2",
-                    user_surname:
-                        "Garrett Winters",
 
-                },
-            ];
+            var sourceData = @json($search_users);
 
         </script>
 
@@ -65,19 +69,41 @@ $search_users = User::all()
                     @csrf
                     <x-div-box class="border-gray-200 border-2 rounded">
 
-                        <div class=" rounded-md border-2 flex-col w-1/3" x-data="filterUser()">
-                            <input class="w-full flex-col" type="search" x-model="search"
-                                   placeholder="Search for the user..."/>
-                            <div class="flex-col">
-                                <template x-for="item in filteredUser" :key="item">
-                                    <div class="flex">
-                                        <p x-text="item.user_surname"></p>
-                                    </div>
+                        <div class=" rounded-md  flex-col w-1/3 p-2 " x-data="filterUser()">
+                            <input class="w-full flex-col "
+                                   type="search"
+                                   x-model="search" placeholder="Search for User"
+                                   @click.away="reset()"
+                                   x-on:keyup.escape="reset()"
+                                   x-on:keyup.down="selectNextUser()"
+                                   x-on:keyup.up="selectPreviousUser()"
+
+                            />
+                            <div class="overflow-y-auto max-h-52 border-2" x-show="filteredUser.length>0">
+                                <template x-for="(item, index) in filteredUser">
+                                    <option class=" p-2   rounded-md hover:bg-indigo-100"
+                                            @click="$dispatch('selected-user' , item)"
+                                            x-text="item.user_name + ' ' + item.user_surname"
+                                            :class="{'bg-indigo-100': index===selectedUserIndex}">
+                                    </option>
+
                                 </template>
+                            </div>
+                            <div class="flex p-2  border-2 rounded-md" x-show="item =! ''">
+                                No Users Available
                             </div>
                         </div>
 
 
+                        <div>
+                            <div x-data="{id: ''}" class="w-1/3 mb-3 hidden h-10" @selected-user.window="id = $event.detail.user_id"  id="id" ></div>
+                            <div>User Name</div>
+                            <div x-data="{name: ''}" class="w-1/3 mb-3 h-10" @selected-user.window="name = $event.detail.user_name" type="text" name="name" x-text="name" ></div>
+                            <div>User Surname</div>
+                            <div x-data="{surname: ''}" class="w-1/3 mb-3 h-10" @selected-user.window="surname = $event.detail.user_surname" type="text" name="surname" x-text="surname" ></div>
+                            <div>User Email</div>
+                            <div x-data="{email: ''}" class="w-1/3 mb-3 h-10" @selected-user.window="email = $event.detail.user_email" type="text" name="email" x-text="email" ></div>
+                        </div>
                         <x-users.form.label for="services" class="text-lg">Select the Services</x-users.form.label>
                         @foreach(\App\Models\Service::pluck('name', 'id') as $service_id => $service_label)
                             <label>
@@ -104,4 +130,5 @@ $search_users = User::all()
     </div>
 
 </x-app-layout>
+
 
